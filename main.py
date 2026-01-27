2100c5f6c30b6a874519d48c200c1fc96437cbb9
Luca Hines <luca.hines@protonmail.com>
2026-01-27 19:34:41 +1100

    added answer lists

---
+++ main.py
@@ -0,0 +1,9 @@
+import discord
+import random
+
+#constants - a list of all possible answers accurate to the game
+responses-positive = ["i guess so...","i think it's fine...",'if you wanna...',"okay...","s...sure...","y..yeah...","y-yeah, definitely!","yes!"]
+responses-negative = ["    d-definitely not...","don't do it...","i think that's a bad idea...","nah...","no.","NO!!!!!!","no no no no no","n-nuh uh...","p...please don't..."]
+responses-uncertain = [    "i dunno","if i say yes, will you take me with you?","i'm not comfortable answering that...","i'm... not sure...","maaaaaaaaybe","uh...uhhhhh...","you should ask your friends for help!"]
+responses-irrelevant = ["i miss my wife", "i am bingbong"]
+
