01ae80fa28d0532c90c95a490c40c755c299e627
Luca Hines <luca.hines@protonmail.com>
2026-01-27 19:42:23 +1100

    fixed variable names issue

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+#!/bin/python3
 import discord
 import random
 
 #constants - a list of all possible answers accurate to the game
-responses-positive = ["i guess so...","i think it's fine...",'if you wanna...',"okay...","s...sure...","y..yeah...","y-yeah, definitely!","yes!"]
-responses-negative = ["    d-definitely not...","don't do it...","i think that's a bad idea...","nah...","no.","NO!!!!!!","no no no no no","n-nuh uh...","p...please don't..."]
-responses-uncertain = [    "i dunno","if i say yes, will you take me with you?","i'm not comfortable answering that...","i'm... not sure...","maaaaaaaaybe","uh...uhhhhh...","you should ask your friends for help!"]
-responses-irrelevant = ["i miss my wife", "i am bingbong"]
+
+responsesPositive = ["i guess so...", "i think it's fine...", "if you wanna...", "okay...", "s...sure...", "y..yeah...", "y-yeah, definitely!", "yes!"]
+responsesNegative = ["    d-definitely not...","don't do it...","i think that's a bad idea...","nah...","no.","NO!!!!!!","no no no no no","n-nuh uh...","p...please don't..."]
+responsesUncertain = [    "i dunno","if i say yes, will you take me with you?","i'm not comfortable answering that...","i'm... not sure...","maaaaaaaaybe","uh...uhhhhh...","you should ask your friends for help!"]
+responsesIrrelevant = ["i miss my wife", "i am bingbong"]
 
